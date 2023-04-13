6d54594e96438738a978f425e57453517bafe2fc
Dan Patton <patton.dan@gmail.com>
2023-04-13 22:40:13 +0100

    Emulate output format of qperft

--- chessica-engine/src/main.rs
+++ chessica-engine/src/main.rs
@@ -13,13 +13,18 @@ fn main() {
         Some(command) => {
             match command.as_str() {
                 "perft" => {
-                    let depth: u8 = args.get(2).and_then(|d| d.parse().ok()).expect("Usage: perft <depth> <fen>");
+                    let maxdepth: u8 = args.get(2).and_then(|d| d.parse().ok()).expect("Usage: perft <depth> <fen>");
                     let fen = args.get(3).expect("Usage: perft <depth> <fen>");
                     let mut board = Board::parse_fen(fen).expect("Invalid Fen");
-                    let start = Instant::now();
-                    let moves = perft(&mut board, depth);
-                    let duration = start.elapsed();
-                    println!("perft({}) moves = {} ({:.2}s)", depth, moves, duration.as_secs_f32());
+                    // ensure magic bitboards are initialised
+                    perft(&mut board, 1);
+                    for i in 0..maxdepth {
+                        let depth = i + 1;
+                        let start = Instant::now();
+                        let moves = perft(&mut board, depth);
+                        let duration = start.elapsed();
+                        println!("perft({:2})= {:12} ( {:.3} sec)", depth, moves, duration.as_secs_f32());
+                    }
                 },
                 _ => {
                     println!("Unknown command: {}", command);
