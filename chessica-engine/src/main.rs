85a27ed8a83c4abec99baacba017f6ebbf84c200
Dan Patton <patton.dan@gmail.com>
2023-04-16 20:50:26 +0100

    Add perft hashing

--- chessica-engine/src/main.rs
+++ chessica-engine/src/main.rs
@@ -3,7 +3,7 @@ extern crate chessica;
 use std::env;
 use std::process::exit;
 use chessica::board::Board;
-use chessica::perft::perft;
+use chessica::perft::{perft, perft_h, PerftHashEntry};
 use std::time::Instant;
 
 fn main() {
@@ -13,17 +13,47 @@ fn main() {
         Some(command) => {
             match command.as_str() {
                 "perft" => {
-                    let maxdepth: u8 = args.get(2).and_then(|d| d.parse().ok()).expect("Usage: perft <depth> <fen>");
-                    let fen = args.get(3).expect("Usage: perft <depth> <fen>");
-                    let mut board = Board::parse_fen(fen).expect("Invalid Fen");
-                    // ensure magic bitboards are initialised
-                    perft(&mut board, 1);
-                    for i in 0..maxdepth {
-                        let depth = i + 1;
-                        let start = Instant::now();
-                        let moves = perft(&mut board, depth);
-                        let duration = start.elapsed();
-                        println!("perft({:2})= {:12} ( {:.3} sec)", depth, moves, duration.as_secs_f32());
+                    if args.len() < 4 || args.len() > 5 {
+                        println!("Usage: perft <max_depth> [-H<hash_bits>] <fen>");
+                        exit(-1);
+                    }
+                    let max_depth: u8 = args.get(2).and_then(|d| d.parse().ok()).unwrap();
+                    if max_depth > 7 {
+                        println!("Error: max_depth cannot exceed 7");
+                        exit(-1);
+                    }
+                    let arg3 = args.get(3).unwrap();
+                    if arg3.starts_with("-H") {
+                        let hash_bits: u8 = arg3[2..].parse().unwrap();
+                        if hash_bits > 30 {
+                            println!("Error: hash_bits cannot exceed 30");
+                            exit(-1);
+                        }
+                        let fen = args.get(4).unwrap();
+                        let mut board = Board::parse_fen(fen).expect("Invalid fen");
+                        // ensure magic bitboards are initialised
+                        perft(&mut board, 1);
+                        let mut hash_table = vec![PerftHashEntry(0, 0); 1 << hash_bits];
+                        for i in 0..max_depth {
+                            let depth = i + 1;
+                            let start = Instant::now();
+                            let moves = perft_h(&mut board, depth, &mut hash_table);
+                            let duration = start.elapsed();
+                            println!("perft({:2})= {:12} ( {:.3} sec)", depth, moves, duration.as_secs_f32());
+                        }
+                    }
+                    else {
+                        let fen = arg3;
+                        let mut board = Board::parse_fen(fen).expect("Invalid fen");
+                        // ensure magic bitboards are initialised
+                        perft(&mut board, 1);
+                        for i in 0..max_depth {
+                            let depth = i + 1;
+                            let start = Instant::now();
+                            let moves = perft(&mut board, depth);
+                            let duration = start.elapsed();
+                            println!("perft({:2})= {:12} ( {:.3} sec)", depth, moves, duration.as_secs_f32());
+                        }
                     }
                 },
                 _ => {
--- chessica/src/perft.rs
+++ chessica/src/perft.rs
@@ -1,5 +1,8 @@
 use crate::board::Board;
 
+#[derive(Clone, Copy)]
+pub struct PerftHashEntry(pub u64, pub u64);
+
 pub fn perft(board: &mut Board, depth: u8) -> u64 {
     match depth {
         0 => 1,
@@ -15,6 +18,42 @@ pub fn perft(board: &mut Board, depth: u8) -> u64 {
             count
         }
     }
+}
+
+pub fn perft_h(board: &mut Board, depth: u8, hash_table: &mut Vec<PerftHashEntry>) -> u64 {
+    assert!(depth <= 7);
+    assert!(hash_table.len().is_power_of_two());
+    assert!(hash_table.len() >= 16);
+    let depth_shift = (hash_table.len().trailing_zeros() - 3) as u8;
+    let hash_mask = (1u64 << depth_shift) - 1;
+
+    fn _impl(board: &mut Board, depth: u64, depth_shift: u8, hash_mask: u64, hash_table: &mut Vec<PerftHashEntry>) -> u64 {
+        let hash = board.hash();
+        let idx = ((depth << depth_shift) | (hash & hash_mask)) as usize;
+        let entry = &hash_table[idx];
+        if entry.0 == hash {
+            return entry.1;
+        }
+        let result = match depth {
+            0 => 1,
+            1 => board.legal_moves().len() as u64,
+            _ => {
+                let legal_moves = board.legal_moves();
+                let mut count: u64 = 0;
+                for move_ in legal_moves.iter() {
+                    board.push(move_);
+                    count += _impl(board, depth - 1, depth_shift, hash_mask, hash_table);
+                    board.pop();
+                }
+                count
+            }
+        };
+        let entry = &mut hash_table[idx];
+        *entry = PerftHashEntry(hash, result);
+        result
+    }
+
+    _impl(board, depth as u64, depth_shift, hash_mask, hash_table)
 }
 
 pub fn perft_split(board: &mut Board, depth: u8) -> u64 {
