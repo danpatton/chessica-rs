27d573b2407773eae13b36ee91f8f56807e79cca
Dan Patton <patton.dan@gmail.com>
2023-05-14 21:14:51 +0100

    Split selfplay out into separate module

--- chessica-engine/src/main.rs
+++ chessica-engine/src/main.rs
@@ -1,3 +1,5 @@
+mod selfplay;
+
 extern crate chessica;
 
 use chessica::board::Board;
@@ -10,8 +12,6 @@ use std::process::exit;
 use std::time::Instant;
 use log::LevelFilter;
 use simplelog::{CombinedLogger, Config, WriteLogger};
-use chessica::Side;
-use chessica_engine::search::{Search, TranspositionTable};
 use chessica_engine::uci::UciSession;
 
 fn main() {
@@ -21,52 +21,7 @@ fn main() {
         Some(command) => {
             match command.as_str() {
                 "selfplay" => {
-                    let mut board = Board::starting_position();
-                    loop {
-                        if board.is_draw_by_threefold_repetition() {
-                            if board.side_to_move() == Side::Black {
-                                println!();
-                            }
-                            println!("1/2-1/2 {{draw by threefold repetition}}");
-                            break;
-                        }
-                        if board.is_draw_by_fifty_move_rule() {
-                            if board.side_to_move() == Side::Black {
-                                println!();
-                            }
-                            println!("1/2-1/2 {{draw by fifty move rule}}");
-                            break;
-                        }
-                        let mut search = Search::new(5);
-                        let mut tt = TranspositionTable::new(24);
-                        match search.search(&board, &mut tt) {
-                            Some(move_) => {
-                                if board.side_to_move() == Side::White {
-                                    print!("{}. {}", board.full_move_number(), move_.pgn_spec(&board));
-                                }
-                                else {
-                                    println!(" {}", move_.pgn_spec(&board));
-                                }
-                                board.push(&move_);
-                            },
-                            None => {
-                                if board.side_to_move() == Side::Black {
-                                    println!();
-                                }
-                                if board.is_in_check() {
-                                    let result_spec = match board.side_to_move() {
-                                        Side::White => "0-1",
-                                        Side::Black => "1-0"
-                                    };
-                                    println!("{}", result_spec);
-                                }
-                                else {
-                                    println!("1/2-1/2 {{draw by stalemate}}");
-                                }
-                                break;
-                            }
-                        }
-                    }
+                    selfplay::selfplay();
                 },
                 "bmagics" => {
                     let start = Instant::now();
---
+++ chessica-engine/src/selfplay.rs
@@ -0,0 +1,53 @@
+use chessica::board::Board;
+use chessica::Side;
+use chessica_engine::search::{Search, TranspositionTable};
+
+pub fn selfplay() {
+    let mut board = Board::starting_position();
+    // let mut tt = TranspositionTable::new(24);
+    let mut tt = TranspositionTable::null();
+    loop {
+        if board.is_draw_by_threefold_repetition() {
+            if board.side_to_move() == Side::Black {
+                println!();
+            }
+            println!("1/2-1/2 {{draw by threefold repetition}}");
+            break;
+        }
+        if board.is_draw_by_fifty_move_rule() {
+            if board.side_to_move() == Side::Black {
+                println!();
+            }
+            println!("1/2-1/2 {{draw by fifty move rule}}");
+            break;
+        }
+        let mut search = Search::new(5);
+        match search.search(&board, &mut tt) {
+            Some(move_) => {
+                if board.side_to_move() == Side::White {
+                    print!("{}. {}", board.full_move_number(), move_.pgn_spec(&board));
+                }
+                else {
+                    println!(" {}", move_.pgn_spec(&board));
+                }
+                board.push(&move_);
+            },
+            None => {
+                if board.side_to_move() == Side::Black {
+                    println!();
+                }
+                if board.is_in_check() {
+                    let result_spec = match board.side_to_move() {
+                        Side::White => "0-1",
+                        Side::Black => "1-0"
+                    };
+                    println!("{}", result_spec);
+                }
+                else {
+                    println!("1/2-1/2 {{draw by stalemate}}");
+                }
+                break;
+            }
+        }
+    }
+}
